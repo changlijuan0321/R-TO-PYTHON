667d102ba374584678b4b53a2160d5801aae0648
changlijuan0321 <changlijuan0321@163.com>
2017-08-30 10:12:35 +0800

    git commit  -m "little change for runangle"

--- runangle-without-plot.py
+++ runangle-without-plot.py
@@ -9,36 +9,42 @@ import sys
 import os
 import math
 import numpy as np
-import lib.tools
+#import lib.tools
 from subfunctions import *  
 
 def runangle(target, delaysNnodes, lonlatNnodes, rtt):
     nblandmark = len(delaysNnodes)
-    rttNnodes = rtt[goodlandRTT]
-    posminrtt = goodlandRTT.index(rttNnodes)
+    # rtt contains the min RTT between target host and landmarks
+    #TODO
+    goodlandRTT = [1,nblandmark]
+    rttNnodes = [goodlandRTT]
+    # posminrtt = goodlandRTT.index(rttNnodes)
     figure = "Figure-R/location_estimate_of_" + target + ".png"
-
-    #boolpolygone is a variable to test whether it has a polygon or not
+    # we output the result in png format, and create the file .png
+    # boolpolygone is a variable to test whether it has a polygon or not
     boolpolygone = 0
     mindelaysNodes = min(delaysNodes)
     pos = delaysNodes.index(mindelaysNodes)
-    ficslt = "Bonpoint/zonecross-" +  target
-
-    if os.path.exists(ficslt):
-        with open('ficrslt.txt') as f:
-            if len(count.fields(ficslt) == 0):
-                #count.fields 计算每行的字段数?
+    ficrslt = "Bonpoint/zonecross-" +  target + ".png"
+    # we test if the file zonecross exits which expressed the targeted host is localizable
+    if os.path.exists(ficrslt):
+        with open(ficrslt, 'w') as f:
+            # we test if we have no crossing point
+            if os.path.getsize('ficrslt'):
+                # TODO count.fields 计算每行的字段数
                 with open('Rslt_localisation/estimate-loc.dat') as f:
                     f.write([target, lonlatNnodes[pos, 0], lonlatNnodes[pos, 1]])
                 LONGITUDE = lonlatNnodes[pos, 0]
                 LATITUDE = lonlatNnodes[pos, 1]
                 with open('Rslt_localisation/rayon-centroid-v2.dat') as f:
                     f.write([target, lonlatNnodes[pos]])
+                # calculation surface of the circle
                 AIRE = math.pi*(delaysNnodes[pos]**2)
                 with open('Rslt_localisation/aire-polygone-v2.dat') as f:
                     f.write([target, lonlatNnodes[pos]])
                 rayon = delaysNnodes[pos]
                 boolpolygone = 2
+            # we have crossing points
             else:
                 pointcross = np.asmatrix(f.read())   
                 long = len(pointcross[:, 1])
@@ -63,12 +69,18 @@ def runangle(target, delaysNnodes, lonlatNnodes, rtt):
                                 pointordonne[compteur, 0] = pointcross[k, 0]
                                 pointordonne[compteur, 1] = pointcross[k, 1]
                         compteur = compteur + 1
-       
+                    # calculate the area and center of the polygon(Cx,Cy), R=sqrt(Aire/pi)
+                    # convert degrees to km (lon,lat)->(dlon*lon,dlat*lat)
+                    # dlon and dlat represent the conversion coefficients for each polygon
                     pointenKM = degre2km(pointordonne)
                     pointendegre = pointordonne
+                    # calculate of the centroid of the polygon and the radius of the circle of this polygon, airepolygone
+                    # is the function that returns the centroid, radius and area of the polygon, and traces the vertices of the polygon
                     CxCyRAIRE = airepolygone(pointendegre,pointenKM)
+
                     #如果面积不为0
                     if CxCyRAIRE[3] != 0 and CxCyRAIRE[3] != 1:
+                        # we trace the centroid of the polygon (estimate of the location of the point)
                         with open('Rslt_localisation/estimate-loc.dat') as f:
                             f.write([target, formatC(CxCyRAIRE[0], digits = 2, format = "f"), formatC(CxCyRAIRE[1], digits = 2, format = "f")])
 
@@ -78,6 +90,7 @@ def runangle(target, delaysNnodes, lonlatNnodes, rtt):
                             f.write([target, CxCyRAIRE[2]])
                         with open('Rslt_localisation/aire-polygone-v2.dat') as f:
                             f.write([target, CxCyRAIRE[3]])
+                        # we have a polygon we put the variable a 1
                         boolpolygone = 1
 
                     elif CxCyRAIRE[3] == 1:
@@ -92,7 +105,8 @@ def runangle(target, delaysNnodes, lonlatNnodes, rtt):
                             f.write([target, rayon])   
                         with open('Rslt_localisation/aire-polygone-v2.dat') as f:
                             f.write([target, aire])
-
+                    # if AIRE == 0 we have identical points in our matrix and in fact we have only 2 points or 1 point we
+                    # look for the barycentre
                     else:
                         for zt in range(1,long):
                             D = calculdist(deg2rad(pointcross[0, 0]), deg2rad(pointcross[0, 1]), deg2rad(pointcross[zt,0]), deg2rad(pointcross[zt,1]))
@@ -170,6 +184,7 @@ def runangle(target, delaysNnodes, lonlatNnodes, rtt):
                         LONGITUDE = lonlatNnodes[pos, 0]
                         LATITUDE = lonlatNnodes[pos, 1]
                         boolpolygone = 2
+    # ficslt 文件不存在，即无法定位
     else:
         with open('Rslt_localisation/rayon-centroid-v2.dat') as f:
             f.write([target, 0])  
@@ -177,7 +192,19 @@ def runangle(target, delaysNnodes, lonlatNnodes, rtt):
             f.write([target, 0])
         with open('Rslt_localisation/estimate-loc.dat') as f:
             f.write([target, 0])
-    
+
+
+
+if __name__ == '__main__':
+    target = "123.123.123.123"
+    delaysNodes = [10, 20, 30]
+    lonlatNnodes = np.array([
+        [30.01, 50.21],
+        [40.01, 60.21],
+        [10.01, 20.21]
+    ])
+    rtt = 12
+    runangle(target, delaysNodes, lonlatNnodes,rtt)
         
 
 
